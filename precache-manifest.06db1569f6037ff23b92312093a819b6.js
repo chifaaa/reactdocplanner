ebe8f90483d39f220c6fb8345cbefac1a790556f
Murodjon <tmurodon@gmail.com>
2019-10-18 16:18:19 +0100

    Updates

---
+++ precache-manifest.06db1569f6037ff23b92312093a819b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0ae7ed4528eeacb1f669fa1098ae2b",
+    "url": "/reactdocplanner/index.html"
+  },
+  {
+    "revision": "dd0056a01394b619c94d",
+    "url": "/reactdocplanner/static/css/main.70c8d22c.chunk.css"
+  },
+  {
+    "revision": "8ec574c95398d3fad2eb",
+    "url": "/reactdocplanner/static/js/2.391dbbc9.chunk.js"
+  },
+  {
+    "revision": "dd0056a01394b619c94d",
+    "url": "/reactdocplanner/static/js/main.02147cea.chunk.js"
+  },
+  {
+    "revision": "cb153a4af2b028236d6e",
+    "url": "/reactdocplanner/static/js/runtime-main.b60d0a71.js"
+  },
+  {
+    "revision": "fee779e1f1907bef78d7aca617c883fa",
+    "url": "/reactdocplanner/static/media/logo.fee779e1.png"
+  }
+]);
